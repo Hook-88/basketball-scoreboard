d7b1dc899550fce2fadeeab83a090cb1717a8b23
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-24 07:59:35 -0500

    Add logic for new-game button

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 //get homescore
+let homeScoreElement = document.getElementById('home-score')
+let homeScore = 0
 //get guestscore
-//listen to click events on buttons
-//update score
-//display updated scores
+let guestScoreElement = document.getElementById('guest-score')
+let guestScore = 0
+//function to update scores
+function add1(team) {
+  if (team === 'HOME') {
+    homeScore += 1;
+    homeScoreElement.textContent = homeScore
+  } else {
+    guestScore += 1;
+    guestScoreElement.textContent = guestScore
+  }
+}
+
+function add2(team) {
+  if (team === 'HOME') {
+    homeScore += 2;
+    homeScoreElement.textContent = homeScore
+  } else {
+    guestScore += 2;
+    guestScoreElement.textContent = guestScore
+  }
+}
+
+function add3(team) {
+  if (team === 'HOME') {
+    homeScore += 3;
+    homeScoreElement.textContent = homeScore
+  } else {
+    guestScore += 3;
+    guestScoreElement.textContent = guestScore
+  }
+}
+//function for reset-btn
+function resetGame() {
+  homeScore = 0
+  homeScoreElement.textContent = homeScore
+  guestScore = 0
+  guestScoreElement.textContent = guestScore
+}
+
